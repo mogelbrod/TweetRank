87c9c72e0eb1361745cf988b2809d95761367511
Joan Puigcerver <joapuipe@gmail.com>
2012-04-24 16:56:59 +0200

    TweetRanker update

--- ranker/ranker/TweetRanker.java
+++ ranker/ranker/TweetRanker.java
@@ -15,7 +15,8 @@ public class TweetRanker {
 	private Hashtable<String, List<String>> userTweets = new Hashtable<String, List<String>>();
 	
 	/** Contains all tweets */
-	private HashSet<String> tweets = new HashSet<String>();
+	private HashSet<String> tweet_set = new HashSet<String>();
+        private ArrayList<String> tweets = new ArrayList<String>();
 		
 	/** Maps a tweet to a list of user mentions */
 	private Hashtable<String, List<String>> mentioned = new Hashtable<String, List<String>>();
@@ -23,44 +24,62 @@ public class TweetRanker {
 	/** Maps a user to a list of users he/she follows */
 	private Hashtable<String, List<String>> follows = new Hashtable<String, List<String>>();
 	
-	/** Map a reply/retweet to the original tweet*/
+	/** Map a reply/retweet to the original tweet */
 	private Hashtable<String, String> refTweets = new Hashtable<String, String>();
+
+        /** Map a tweet to a list of hashtags */
+        private Hashtable<String, List<String>> tweetHashtags = new Hashtable<String, List<String>>();
 	
 	public void computePageRank() {
 	    int n = tweets.size();
 	    int m = n/10;
 	    MCCompletePath(n, m);
 	}
-	
-        /*public void parseTweets(String tweetString) {
-		Collections.addAll(tweets, tweetString.split(","));
-	}*/
+
+        private void addTweet(String tweetID) {
+	    if ( !tweet_set.contains(tweetID) ) {
+	        tweet_set.add(tweetID);
+		tweets.add(tweetID);
+	    }
+	}
+
+        private void addAllTweets(List<String> tweetIDs) {
+	    for (String tid : tweetIDs)
+		addTweet(tid);
+        } 
 	
 	public void addRefTweets(String tweetID, String refTweetID) {
-		refTweets.put(tweetID, refTweetID);
-		tweets.add(tweetID);
-		tweets.add(refTweetID);
+	    refTweets.put(tweetID, refTweetID);
+	    addTweet(tweetID);
+	    addTweet(refTweetID);
 	}
 	
 	public void addUserTweets(String userID, List<String> tweetIDs) {
 	    List<String> curr_list = userTweets.get(userID);
-	    if (curr_list == null)
-		userTweets.put(userID, tweetIDs);
-	    else {
-		curr_list.addAll(tweetIDs);
-		//userTweets.put(userID, curr_list);
-	    }
+	    if (curr_list == null) userTweets.put(userID, tweetIDs);
+	    else curr_list.addAll(tweetIDs);
+	    addAllTweets(tweetIDs);
 	}
 	
 	public void addMentioned(String tweetID, List<String> userIDs) {
-	    //List<String> l = Arrays.asList(userString.split(","));
-	    //mentioned.put(tweetID, l);
+	    List<String> curr_list = mentioned.get(tweetID);
+	    if (curr_list == null) mentioned.put(tweetID, userIDs);
+	    else curr_list.addAll(userIDs);
+	    addTweet(tweetID);
 	}
 	
 	public void addFollows(String userID, List<String> userIDs) {
-	    //List<String> l = Arrays.asList(userString.split(","));
-	    //	follows.put(userID, l);
+	    List<String> curr_list = follows.get(userID);
+	    if (curr_list == null) follows.put(userID, userIDs);
+	    else curr_list.addAll(userIDs);
 	}
+
+        public void addHashtags(String tweetID, List<String> hashtags) {
+	    List<String> curr_list = tweetHashtags.get(tweetID);
+	    if (curr_list == null) tweetHashtags.put(tweetID, hashtags);
+	    else curr_list.addAll(hashtags);
+	    addTweet(tweetID);
+        }
 	
 	private void addVisit(String tweetID) {
 		if(tweetVisited.containsKey(tweetID))
