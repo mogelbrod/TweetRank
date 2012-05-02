db0c8ede8290b8f30c35ad35ed84235a49eb5be4
Joan Puigcerver <joapuipe@gmail.com>
2012-05-02 17:40:02 +0200

    mysolr-custom library removed. Not used.

--- crawler/mysolr-custom/mysolr/__init__.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from .mysolr_response import SolrResponse
-from .mysolr import Solr
-
-__title__ = 'mysolr'
-__version__ = '0.5.1'
-__build__ = 0x000051
-__author__ = 'Rubén Abad, Miguel Olivares'
